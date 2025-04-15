8fe584f5c3105731f9d1a221673784fc4c7a0fd7
flanche <flanchegong@163.com>
2025-04-15 16:09:40 +0800

    panic

--- internal/biz/user_test.go
+++ internal/biz/user_test.go
@@ -1,23 +1,13 @@
 package biz
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/davecgh/go-spew/spew"
+)
 
 func TestHashPassword(t *testing.T) {
-	// Test cases for the hashPassword function
-	tests := []struct {
-		input    string
-		expected string
-	}{
-		{"password123", "$2a$10$KIXb7h5jvY1g8m0Z5c3f6OeQ9f9f9f9f9f9f9f9f9f9f9f9f9f9"},
-		{"mypassword", "$2a$10$KIXb7h5jvY1g8m0Z5c3f6OeQ9f9f9f9f9f9f9f9f9f9f9f9f9"},
-	}
-
-	for _, test := range tests {
-		t.Run(test.input, func(t *testing.T) {
-			result := hashPassword(test.input)
-			if result != test.expected {
-				t.Errorf("expected %s, got %s", test.expected, result)
-			}
-		})
-	}
+	s := hashPassword("123456")
+	spew.Dump(s)
+	panic(1)
 }
