2792405fec29b76f977c3fd2b54e5a57a3fcb127
flanche <flanchegong@163.com>
2025-04-16 19:37:51 +0800

    jwt

--- internal/server/http.go
+++ internal/server/http.go
@@ -1,21 +1,38 @@
 package server
 
 import (
+	"context"
+
 	v1 "github.com/go-kratos/kratos-layout/api/realworld/v1"
 	"github.com/go-kratos/kratos-layout/internal/conf"
+	"github.com/go-kratos/kratos-layout/internal/pkg/middleware/auth"
 	"github.com/go-kratos/kratos-layout/internal/service"
 
 	"github.com/go-kratos/kratos/v2/log"
 	"github.com/go-kratos/kratos/v2/middleware/recovery"
+	"github.com/go-kratos/kratos/v2/middleware/selector"
 	"github.com/go-kratos/kratos/v2/transport/http"
 )
+
+func NewSkipRouterMatcher() selector.MatchFunc {
+
+	skipRouter := make(map[string]struct{})
+	skipRouter["/realworld.v1.RealWorld/Login"] = struct{}{}
+	skipRouter["/realworld.v1.RealWorld/Register"] = struct{}{}
+	return func(ctx context.Context, operation string) bool {
+		if _, ok := skipRouter[operation]; ok {
+			return false
+		}
+		return true
+	}
+}
 
 // NewHTTPServer new an HTTP server.
 func NewHTTPServer(c *conf.Server, jwtc *conf.JWT, realworld *service.RealWorldService, logger log.Logger) *http.Server {
 	var opts = []http.ServerOption{
 		http.Middleware(
 			recovery.Recovery(),
-			//auth.JWTAuth(jwtc.Token),
+			selector.Server(auth.JWTAuth(jwtc.Token)).Match(NewSkipRouterMatcher()).Build(),
 		),
 	}
 	if c.Http.Network != "" {
